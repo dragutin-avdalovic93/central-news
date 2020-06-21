29dbb078f1909d60d533eb2b1f481958ea5dfb5b
dragutin-avdalovic93 <dragutinavdalovic@gmail.com>
2020-06-21 22:39:05 +0200

    ads added money is here

--- layouts/blog.vue
+++ layouts/blog.vue
@@ -7,6 +7,19 @@
       <div class="row main-row">
         <div class="col-12 col-md-2 col-lg-2 banners order-1 order-sm-1 order-xs-1 order-md-0 order-lg-0 order-xl-0">
           <div class="divider-15"></div>
+          <div><a href="https://www.facebook.com/skiravnaplanina/" title="Ski centar 'Ravna Planina'" target="_blank" ><img src="https://admincentralnews.xyz/wp-content/uploads/2020/06/ski_centar.jpg" class="img-fluid-ad" alt="Ski centar 'Ravna Planina'"></a></div>
+          <div class="divider-15"></div>
+          <div><a href="https://m.facebook.com/HotelLavinaJahorina/" title="Hotel Lavina Jahorina" target="_blank" >
+            <video width="100%" height="auto" autoplay muted class="img-fluid-ad">
+              <source src="https://admincentralnews.xyz/wp-content/uploads/2020/06/lavina.mp4" type="video/mp4">
+              Your browser does not support the video tag.
+            </video>
+          </a></div>
+           <div class="divider-15"></div>
+          <div><a href="https://www.facebook.com/Al-Parco-ristorante-1709486515947045/" title="Al Parco Ristorante" target="_blank"><img src="https://admincentralnews.xyz/wp-content/uploads/2020/06/al_parco.jpg" class="img-fluid-ad" alt="Al Parco Ristorante"></a></div>
+          <div class="divider-15"></div>
+          <div><a href="https://www.facebook.com/zlatniborhotel/" title="Hotel Zlatni Bor" target="_blank"><img src="https://admincentralnews.xyz/wp-content/uploads/2020/06/zlatni_bor.jpg" class="img-fluid-ad" alt="Hotel Zlatni Bor"></a></div>
+          <div class="divider-15"></div>
           <div><a href="" title="Mjesto za vašu reklamu" target="_self"><img src="https://admincentralnews.xyz/wp-content/uploads/2020/05/ad.png" class="img-fluid-ad" alt="Mjesto za reklamu"></a></div>
           <div class="divider-15"></div>
         </div>
